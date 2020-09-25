70ec01d1917ca4031b3876b219f127a180f16d7a
Steven Personal <stevepopovich8@gmail.com>
2020-09-25 00:54:06 -0400

    Dont test build config

--- app/src/test/java/com/stevenpopovich/talktothat/UtilTests.kt
+++ app/src/test/java/com/stevenpopovich/talktothat/UtilTests.kt
@@ -12,7 +12,6 @@ class UtilTests {
     @Test
     fun testBuildConfig() {
         assertEquals("com.stevenpopovich.talktothat", BuildConfig.APPLICATION_ID)
-        assertEquals("debug", BuildConfig.BUILD_TYPE)
         assertEquals(true, BuildConfig.DEBUG)
         assertEquals(1, BuildConfig.VERSION_CODE)
         assertEquals("1.0", BuildConfig.VERSION_NAME)
