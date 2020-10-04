8d5cdbaa9653ba8bfd9b88a37ad9e7c008d06113
Steven Personal <stevepopovich8@gmail.com>
2020-10-04 18:47:25 -0400

    Rectangle drawable tests

--- app/src/main/java/com/stevenpopovich/talktothat/objecttracker/RectangleDrawable.kt
+++ app/src/main/java/com/stevenpopovich/talktothat/objecttracker/RectangleDrawable.kt
@@ -8,15 +8,11 @@ import android.graphics.PixelFormat
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 
-class RectangleDrawable(private val drawRect: Rect) : Drawable() {
-    private val paint = Paint()
-
-    init {
+class RectangleDrawable(private val drawRect: Rect, private val paint: Paint = Paint()) : Drawable() {
+    override fun draw(canvas: Canvas) {
         paint.style = Paint.Style.STROKE
         paint.color = Color.YELLOW
-    }
 
-    override fun draw(canvas: Canvas) {
         canvas.drawRect(drawRect, paint)
     }
 
