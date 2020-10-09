e29af713ea6031c95dda5724a6305b61f44ce45b
Steven Personal <stevepopovich8@gmail.com>
2020-10-09 19:35:23 -0400

    This works with https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/

--- arduino/power_motors/sketch_oct05a/sketch_oct05a.ino
+++ arduino/power_motors/sketch_oct05a/sketch_oct05a.ino
@@ -1,29 +1,40 @@
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+// Motor B connections
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
 
-int leftPWMPort = 5;
-int rightPWMPort = 10;
-
-int leftDigital1 = 6;
-int leftDigital2 = 7;
-
-int rightDigital1 = 9;
-int rightDigital2 = 8;
+int speed = 0;
 
 void setup() {
-  pinMode(leftPWMPort, OUTPUT);
-  pinMode(leftDigital1, OUTPUT);
-  pinMode(leftDigital2, OUTPUT);
-  pinMode(rightPWMPort, OUTPUT);
-  pinMode(rightDigital1, OUTPUT);
-  pinMode(rightDigital2, OUTPUT);
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 void loop() {
-  digitalWrite(leftDigital1, LOW);
-  digitalWrite(leftDigital2, HIGH);
-  digitalWrite(rightDigital1, HIGH);
-  digitalWrite(rightDigital2, LOW);
+  analogWrite(enA, speed);
+  analogWrite(enB, speed);
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
+  speed++;
   
-  analogWrite(leftPWMPort, 255);
-  analogWrite(rightPWMPort, 255);
-  delay(1000);
+  delay(250);
 }
