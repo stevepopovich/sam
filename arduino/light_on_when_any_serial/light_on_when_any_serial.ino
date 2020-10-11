ca55e7beb958be30f47cb0f8a45d12aac938f399
Steven Personal <stevepopovich8@gmail.com>
2020-10-11 17:01:48 -0400

    Read and hopefully print arduino serial

--- arduino/light_on_when_any_serial/light_on_when_any_serial.ino
+++ arduino/light_on_when_any_serial/light_on_when_any_serial.ino
@@ -6,11 +6,47 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  while (Serial.available() > 0) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.read();
-    delay(250);
-   }
+//  while (Serial.available() > 0) {
+//    digitalWrite(LED_BUILTIN, HIGH);
+//    incoming = Serial.readString();
+//    // say what you got:
+//    Serial.println(incoming);  
+//    delay(250);
+//   }
+//
+//  digitalWrite(LED_BUILTIN, LOW);
 
-  digitalWrite(LED_BUILTIN, LOW);
+  recvOneChar();
+  showNewData();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+   
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChar);
+        newData = false;
+    }
 }
