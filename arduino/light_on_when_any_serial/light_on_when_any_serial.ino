4374533eb4f9f19d47200bd8c8d742e3a06bf446
Steven Popovich <stevepopovich8@gmail.com>
2020-11-15 21:34:01 -0500

    When we get on, drive forward (#48)

--- arduino/light_on_when_any_serial/light_on_when_any_serial.ino
+++ arduino/light_on_when_any_serial/light_on_when_any_serial.ino
@@ -1,15 +1,37 @@
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
 const byte numChars = 64;
-char receivedChars[numChars];   // an array to store the received data
+char receivedChars[numChars];
 
 boolean newData = false;
 
 void setup() {
     Serial.begin(9600);
+
+    // Set all the motor control pins to outputs
+    pinMode(enA, OUTPUT);
+    pinMode(enB, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+    
+    // Turn off motors - Initial state
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
 }
 
 void loop() {
     recvWithEndMarker();
-    delay(400);
+    delay(1000);
     showNewData();
 }
 
@@ -39,6 +61,17 @@ void recvWithEndMarker() {
 void showNewData() {
     if (newData == true) {
         Serial.println(receivedChars);
+        Serial.println(strcmp(receivedChars, "on"));
+
+        if (strcmp(receivedChars, "on") == 0) {
+            analogWrite(enA, 255);
+            analogWrite(enB, 255);
+          
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+        }
         newData = false;
     }
 }
