48d4d25e58230e91f769bceb5a2b096af66c494a
Steven Personal <stevepopovich8@gmail.com>
2020-11-30 21:37:27 -0500

    Reorganize a bunch of stuff

--- arduino/main_logic/main_logic.ino
+++ arduino/main_logic/main_logic.ino
@@ -96,7 +96,7 @@ void readDistance() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (back and forth)
   // Displays the distance on the Serial Monitor
 //  Serial.print("Distance: ");
 //  Serial.print(distance);
