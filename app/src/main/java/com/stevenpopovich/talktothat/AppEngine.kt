844b024f93f5d6cea83d0b54e0cb3fe9e725744b
Steven Personal <stevepopovich8@gmail.com>
2020-12-01 21:43:33 -0500

    Add back in vision tracking

--- app/src/main/java/com/stevenpopovich/talktothat/AppEngine.kt
+++ app/src/main/java/com/stevenpopovich/talktothat/AppEngine.kt
@@ -1,6 +1,7 @@
 package com.stevenpopovich.talktothat
 
 import android.content.Intent
+import com.stevenpopovich.talktothat.cameraengine.CameraEngine
 import com.stevenpopovich.talktothat.speechrecognition.ContinuousSpeechRecognitionListener
 import com.stevenpopovich.talktothat.speechrecognition.ContinuousSpeechRecognizer
 import com.stevenpopovich.talktothat.speechrecognition.SpeechResultsBusinessLogicEngine
@@ -9,6 +10,7 @@ class AppEngine {
     fun start(
         continuousSpeechRecognizer: ContinuousSpeechRecognizer,
         logicEngine: SpeechResultsBusinessLogicEngine,
+        cameraEngine: CameraEngine,
         intent: Intent = Intent(),
         recognitionListener: ContinuousSpeechRecognitionListener = ContinuousSpeechRecognitionListener(
             logicEngine::onSpeechResults
@@ -18,5 +20,7 @@ class AppEngine {
             intent,
             recognitionListener
         )
+
+        cameraEngine.start()
     }
 }
