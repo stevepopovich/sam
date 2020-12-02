2721b15577be70f968abf121da7cfe939d72c8b1
Steven Personal <stevepopovich8@gmail.com>
2020-12-01 21:42:48 -0500

    make Sam stop when we are about to run into something

--- arduino/main_logic/main_logic.ino
+++ arduino/main_logic/main_logic.ino
@@ -37,7 +37,8 @@ void loop() {
     recvWithEndMarker();
     processData();
     readDistance();
-    delay(250);
+    stopIfDistanceIsShort();
+    delay(50);
 }
 
 void recvWithEndMarker() {
@@ -97,10 +98,15 @@ void readDistance() {
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (back and forth)
-  // Displays the distance on the Serial Monitor
 //  Serial.print("Distance: ");
 //  Serial.print(distance);
 //  Serial.println(" cm");
+}
+
+void stopIfDistanceIsShort() {
+  if (distance < 10) {
+    stopMoving();  
+  }
 }
 
 void spinCounterClockwise() {
