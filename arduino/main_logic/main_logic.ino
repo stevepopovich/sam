da8990a5b593c3dd7fd8d3674283eacbde1ae88d
Steven Personal <stevepopovich8@gmail.com>
2021-04-01 21:38:14 -0400

    Reinstall ultrasonic and code

--- arduino/main_logic/main_logic.ino
+++ arduino/main_logic/main_logic.ino
@@ -2,8 +2,8 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-const int trigPin = 13;// these will not work
-const int echoPin = 12; // the motor shield or limit switches are using these
+const int trigPin = 10;
+const int echoPin = 11;
 
 const byte numChars = 64;
 char receivedChars[numChars];
@@ -12,14 +12,13 @@ boolean newData = false;
 
 int currStep = 0;
 
+int distance = 0;
+long duration = 0;
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
 Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-Adafruit_StepperMotor *stepper = AFMS.getStepper(200, 2);
-
-int limitSwitchUpper = 13; // this one works great
-int limitSwitchLower = 0; // analog read 
 
 void setup() {
     Serial.begin(9600);
@@ -28,36 +27,16 @@ void setup() {
     
     stopMoving();
 
-    stepper->setSpeed(1);
-    stepper->step(1, FORWARD, DOUBLE);
-
-    pinMode(limitSwitchUpper, INPUT); // 0 is closed
-    pinMode(limitSwitchLower, INPUT); // 0 is closed
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void loop() {
     recvWithEndMarker();
-    processData();
-//    readDistance();
-  //    stopIfDistanceIsShort(); // Having the phone near the ultrasonic is causing the ultrasonic to short, reading zero
-//    Serial.println(digitalRead(limitSwitchUpper)); 
-//    Serial.println(analogRead(limitSwitchLower)); 
-
-//    stepper->step(1, FORWARD, DOUBLE); // foward is down
-    articulateFaceIfLegal(-200);
+//    processData();
+    readDistance();
+    goForward();
 }
-
-void articulateFaceIfLegal(int value) { // positive is look down
-  if (value > 0 && analogRead(limitSwitchLower) != 0) {
-    stepper->setSpeed(value);
-    stepper->step(1, FORWARD, DOUBLE);
-  } else if (value < 0 && digitalRead(limitSwitchUpper) != 0){
-    stepper->setSpeed(abs(value));
-    stepper->step(1, BACKWARD, DOUBLE);
-  } else  {
-     // Do I need to do anything?
-  }
-} 
 
 void recvWithEndMarker() {
     static byte ndx = 0;
@@ -98,7 +77,7 @@ void processData() {
         }
         else if (strcmp(receivedChars, "stop") == 0) {
           stopMoving();
-        } else { // expecting just a number `-255/-255` - `255/255`
+        } else { // expecting a number -255 to 255
           spinByNumber(atoi(receivedChars));
         }
         
@@ -115,26 +94,21 @@ void readDistance() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-//  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-//  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (back and forth)
-//  Serial.print("Distance: ");
-//  Serial.print(distance);
-//  Serial.println(" cm");
-}
-
-void stopIfDistanceIsShort() {
-//  if (distance < 10) {
-//    stopMoving();  
-//  }
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (back and forth)
 }
 
 void goForward() { 
-  motor1->setSpeed(255);
-  motor2->setSpeed(255);
-
-  motor1->run(FORWARD);
-  motor2->run(FORWARD);
+    if (distance > 15) {
+      motor1->setSpeed(70);
+      motor2->setSpeed(70);
+      
+      motor1->run(FORWARD);
+      motor2->run(FORWARD);
+    } else {
+      stopMoving();  
+    }
 }
 
 void goBackward() {
@@ -143,9 +117,6 @@ void goBackward() {
 
   motor1->run(BACKWARD);
   motor2->run(BACKWARD);
-
-//  stepper->setSpeed(255);
-//  stepper->step(10, FORWARD, MICROSTEP);
 }
 
 void spinClockwise(int spinSpeed) {
